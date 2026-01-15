442100843c5955f530bc757bac973fe2c3f6f657
José Emanuel RG <resendizgonzalezjoseemanuel@gmail.com>
2026-01-15 02:46:08 -0600

    Update: Conectando frontend a producción en Render

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -53,7 +53,7 @@ export default function App() {
 
 
     try {
-      const res = await fetch("http://localhost:3001/api/chat", {
+      const res = await fetch("http://traxion-backend-api.onrender.com/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
